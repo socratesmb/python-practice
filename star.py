e615e85f8173e81029bc27782d1421a9d74e0e04
socratesmb <53800580+socratesmb@users.noreply.github.com>
2022-10-04 15:17:57 -0500

    Primer Codigo

---
+++ star.py
@@ -0,0 +1,10 @@
+# Hola mundo basico solo print, this line is only coment.
+print("Hola Python")
+print('Hola Python')
+
+# Consultar tipo de datos.
+print(type('Hola Mundo')) # Dato tipo 'Str'
+print(type(7)) # Dato tipo 'Int'
+print(type(7.2)) # Dato tipo 'Float'
+print(type(7 + 3j)) # Dato tipo 'Complex'
+print(type(True)) # Dato tipo 'Boolean'
